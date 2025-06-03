b1ba633eb131eb436d79dfa2a21027cf2136f6d7
NicolasGuerrero <nicoguerrero0800@gmail.com>
2025-06-02 18:17:55 -0600

    organizacion del proyecto

---
+++ main.py
@@ -0,0 +1,27 @@
+import sys
+import json
+
+# add expense with a description and amount.
+def add():
+    pass
+
+# update an expense (with id)
+
+def update():
+    pass
+
+# delete an expense (with id)
+
+def delete():
+    pass
+# view a summary of all expenses
+
+def viewAll():
+    pass
+# view a summary for a specific month (of current year)
+def view():
+    pass
+
+
+if __name__ == "__main__":
+    pass
