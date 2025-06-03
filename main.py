6bb1704bb578d1577ab0eeeea7702b4c5d5213ac
NicolasGuerrero <nicoguerrero0800@gmail.com>
2025-06-02 18:55:40 -0600

    add function with json file

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
 import sys
 import json
+from datetime import datetime
+
+#JSON
+JSON_FILE = "expense.json"
+
+#load json
+def load_expense():
+    try:
+        with open(JSON_FILE, "r") as f:
+            return json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return []
+
+# generate new ID
+def new_id(tasks):
+    if not tasks:
+        return 1
+    return max(t['id'] for t in tasks) + 1
 
 # add expense with a description and amount.
-def add():
-    pass
+def add(argv):
+    description = "".join(argv[3])
+    amount = "".join(argv[5])
+    expenses = load_expense()
+    id = new_id(expenses)
+    now = datetime.now().strftime("%Y-%m-%d")
 
+    new_expense = {
+        'id': id,
+        'date': now,
+        'description': description,
+        'amount': amount
+    }
+    expenses.append(new_expense)
+    with open(JSON_FILE, "w") as f:
+        json.dump(expenses, f, indent=4)
+    print(f"\nExpense added successfully (ID: {id})")
 # update an expense (with id)
 
 def update():
@@ -17,11 +49,29 @@ def delete():
 # view a summary of all expenses
 
 def viewAll():
-    pass
+    expenses = load_expense()
+
+    if not expenses:
+        print("no expense")
+        return
+    
+    print(f"{'ID':<5} {'Date':<12} {'Description':<20} {'Amount':>8}")
+    for expense in expenses:
+        print(f"{expense['id']:<5} {expense['date']:<12} {expense['description']:<20} {expense['amount']:>8}")
+
+
 # view a summary for a specific month (of current year)
 def view():
     pass
 
+def main():
+    argv = sys.argv
 
+    command = argv[1]
+
+    if command == "add":
+        add(argv)
+    elif command == "list":
+        viewAll()
 if __name__ == "__main__":
-    pass
+    main()
